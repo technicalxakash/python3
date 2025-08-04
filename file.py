6e11ae2bf7de901b6161fcee42f51e4e07532fcd
technicalxakash <akashr2468@gmail.com>
2025-08-04 22:31:25 +0530

    new

---
+++ file.py
@@ -0,0 +1,33 @@
+
+# print("hello")
+
+# a=10;
+# b=20;
+
+# print(a+b)
+
+# a=input();
+# print("you entered the number is ",a)
+
+# a=100;
+# 100=literals
+
+#constant is a vairable whos value can't be changed
+
+#pi=3.14,pi is a constant and pi is a variable
+
+# print(type(100))
+
+# b=1.5
+# b=int(b);
+# print(type(b))
+
+# a=tuple([10,1,3,4,5,6])
+# b=list(a)
+# b[0]=87
+# print(b)
+
+#print(ord("a"))=prints ascii value
+
+# a=67
+# print(chr(a))=prints alphabets
